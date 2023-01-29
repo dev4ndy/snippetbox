60d95adaa9402d95e965ba5cd1f83147a423fbf4
Andres Echeverri <andrex.125d@gmail.com>
2023-01-29 11:52:48 -0500

    Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+
+	var cfg config
+
+	// command-line flags
+	// To change the Http network default address, use: `go run main.go -addr=":80"`
+	flag.StringVar(&cfg.addr, "addr", ":8080", "Http network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "../../ui/static/", "Path to static assets")
+	// We need to use flag.Parse before to use the flags
+	// otherwise it will take the default value
+	flag.Parse()
+
 	mux := http.NewServeMux()
-	nfs := neuteredFileSystem{http.Dir("../../ui/static/")}
+	nfs := neuteredFileSystem{http.Dir(cfg.staticDir)}
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(nfs)))
 	home := NewHome()
 
@@ -16,8 +33,8 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting Server on : 8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Starting Server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
 
